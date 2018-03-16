437ea8999c30cd4c5d3f452cfdcbbf37c560ad8c
Javier Mantilla <>
2018-03-16 16:09:23 +0000

    Notification initial commit

---
+++ src/main/java/ie/gmit/sw/fyp/notification/Notification.java
@@ -0,0 +1,41 @@
+package ie.gmit.sw.fyp.notification;
+
+public class Notification {
+//	Fields
+	private StringBuilder message;
+
+	
+	
+	
+//	Constructors
+	public Notification() {
+		
+	}
+
+	public Notification(String message) {
+		this.message = new StringBuilder(message);
+	}
+
+	
+	
+	
+//	Accessors and mutators
+	public String getMessage() {
+		return message.toString();
+	}
+
+	public void setMessage(String message) {
+		this.message.setLength(0);
+		
+		this.message.append(message);
+	}
+	
+	
+	
+	
+//	Methods
+	public void updateMessage(String message) {
+		this.message.append(message);
+	} // updateMessage
+	
+} // end class Notification
