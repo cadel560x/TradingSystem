9b223ced20a97a78c3af1e3781830d94780cf7a3
Javier Mantilla <>
2018-03-16 15:05:08 +0000

    Re-created with Spring Boot Starter Project

---
+++ src/main/java/ie/gmit/sw/fyp/TradingSystemApplication.java
@@ -0,0 +1,12 @@
+package ie.gmit.sw.fyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingSystemApplication.class, args);
+	}
+}
